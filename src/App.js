66ea1b8d221e6f2ea1dbbdc11121bbaa49b757ef
Siarhei Miksiuk <smiksiuk@alluma.org>
2020-02-08 12:27:14 -0800

    second

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ function App() {
     link: "bla4",
   }];
 
-  const footerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.";
+  const footerText = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. ";
 
   return (
     <div className="App container-xl">
--- src/Footer.js
+++ src/Footer.js
@@ -5,13 +5,13 @@ import Menu from './Menu'
 
 function Footer(props) {
   return (
-    <footer className="page-footer font-small bg-light pt-3">
-      <div class="container-fluid text-center text-md-left">
-        <div class="row">
-          <div class="col-md-3 mt-md-0 mt-3"><Logo /></div>
-          <div class="col-md-3 mt-md-0 mt-3"><Menu menuItems={props.menu1} /> </div>
-          <div class="col-md-3 mt-md-0 mt-3"><Menu menuItems={props.menu2} /> </div>
-          <div class="col-md-3 mt-md-0 mt-3">{props.text}</div>
+    <footer className="page-footer font-small bg-light">
+      <div className="container-fluid text-center text-md-left">
+        <div className="row">
+          <div className="col-md-3  pt-3"><Logo /></div>
+          <div className="col-md-3"><Menu menuItems={props.menu1} /> </div>
+          <div className="col-md-3"><Menu menuItems={props.menu2} /> </div>
+          <div className="col-md-3">{props.text}</div>
         </div>
       </div>
     </footer>
